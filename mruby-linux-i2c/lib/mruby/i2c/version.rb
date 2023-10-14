346dd12975542c99695718a836f6317b266de78f
HirohitoHigashi <higashi@s-itoc.jp>
2023-10-15 06:52:25 +0900

    update version.

--- mruby-linux-i2c/lib/mruby/i2c/version.rb
+++ mruby-linux-i2c/lib/mruby/i2c/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module I2C
-  VERSION = "0.9.1"
+  VERSION = "0.9.2"
 end
--- mruby-linux-spi/lib/mruby/spi/version.rb
+++ mruby-linux-spi/lib/mruby/spi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module SPI
-  VERSION = "0.9.0"
+  VERSION = "0.9.2"
 end
